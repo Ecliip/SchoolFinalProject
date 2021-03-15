6cb88035d6cec3df2ffbff0c52b176aabb9e4d99
Mika <prodomitor@gmail.com>
2021-03-15 23:14:53 +0100

    links to cars in cart added

--- resources/views/cart.blade.php
+++ resources/views/cart.blade.php
@@ -13,7 +13,11 @@
                 @foreach($carsArr as $car)
 {{--                    @foreach($car as $c_item)--}}
                         <tr class="tr">
-                            <td class="ti-photo"><div class="image-box-sm"><img src="{{asset($car->photo_sm)}}"></div></td>
+                            <td class="ti-photo">
+                                <a href="{{route('info.car', $car->id)}}">
+                                    <div class="image-box-sm"><img src="{{asset($car->photo_sm)}}"></div>
+                                </a>
+                            </td>
                             <td class="ti-carName">{{$car->brand->brand_name}} {{$car->carModel->model_name}}</td>
                             <td>{{$car->price}}</td>
                             <td><a class="a_delete" href="{{route('delete.cart', $car->id)}}">Eliminar</a></td>
@@ -22,5 +26,5 @@
                 @endforeach
         </tbody>
     </table>
-        <span class="totalPrice">The total price is: {{$totalPrice}}</span>
+        <div class="totalPrice">The total price is: {{$totalPrice}}</div>
 @endsection
