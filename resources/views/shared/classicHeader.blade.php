273e76f1b6fe751b34b60edc79fe3e9f8d7b78fa
Mika <prodomitor@gmail.com>
2021-03-15 11:03:52 +0100

    classic header changed

--- resources/views/shared/classicHeader.blade.php
+++ resources/views/shared/classicHeader.blade.php
@@ -20,7 +20,11 @@
                 </div>
                     <div class="right">
                 @auth
-                    <a href="{{ url('/dashboard') }}" class="nav-link">Dashboard</a>
+                    <p class="name">{{Auth::user()->name}}</p>
+                    <a href="{{ url('/dashboard') }}" class="nav-link">Configuración</a>
+                    <a href="{{ route('user.logout') }}" class="nav-link">Salir</a>
+                    <a href="#" class="nav-link">Mi Carrito</a>
+
                 @else
                     <a href="{{ route('login') }}" class="nav-link">Log in</a>
                     @if (Route::has('register'))
