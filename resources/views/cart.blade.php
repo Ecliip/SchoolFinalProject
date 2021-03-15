dd42445a5eaa1241f4d8d48be537861f680428fc
Mika <prodomitor@gmail.com>
2021-03-15 19:10:05 +0100

    cart UI builded

--- resources/views/cart.blade.php
+++ resources/views/cart.blade.php
@@ -1,8 +1,28 @@
 @extends('layouts.classic')
 @section('content')
-    @foreach($carsArr as $car)
-        @foreach($car as $c_item)
-            {{$c_item->id}}
-        @endforeach
-    @endforeach
+    <table>
+        <thead>
+            <tr>
+                <td>Foto</td>
+                <td>Coche</td>
+                <td>Precio</td>
+                <td>Eliminar de la lista</td>
+            </tr>
+        </thead>
+        <tbody>
+
+                @foreach($carsArr as $car)
+                    @foreach($car as $c_item)
+                        <tr class="tr">
+                            <td class="ti-photo"><div class="image-box-sm"><img src="{{asset($c_item->photo_sm)}}"></div></td>
+                            <td class="ti-carName">{{$c_item->brand->brand_name}} {{$c_item->carModel->model_name}}</td>
+                            <td>{{$c_item->price}}</td>
+                            <td><a href="#">Eliminar</a></td>
+                        </tr>
+                    @endforeach
+                @endforeach
+
+
+        </tbody>
+    </table>
 @endsection
