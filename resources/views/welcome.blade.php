4e23a2a64ff56d7b96b9f89c1bb0f3f389d04251
Mikhaylo <prodomitor@gmail.com>
2021-04-02 19:27:53 +0200

    added alt to images in welcome page

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,5 +1,6 @@
 @extends('layouts.classic')
 @section('content')
+{{--        PRINCIPIO SECCION CARROCERIAS       --}}
     @if($categories->count() > 0)
         <section>
             <h2>Carrocerias mas populares</h2>
@@ -9,16 +10,17 @@
                             <div class="card-25">
                                 <h3>{{$category->category}}</h3>
                                 <div class="image-box-sm">
-                                    <img src="{{$category->photo_sm}}">
+                                    <img src="{{$category->photo_sm}}" alt="Carroceria tipo {{$category->cat_name}}">
                                 </div>
                             </div>
                         </a>
                     @endforeach
-
                 </div>
             <a class="open-all" href="{{route('all.car')}}">Ver todas las carrocerias</a>
         </section>
     @endif
+{{--        FIN SECCION CARROCERIAS       --}}
+{{--        PRINCIPIO SECCION MARCAS       --}}
     @if($brands->count() > 0)
         <section>
             <h2>Marcas mas populares</h2>
@@ -28,7 +30,7 @@
                         <div class="card-25">
                             <h3>{{$brand->brand}}</h3>
                             <div class="image-box-sm">
-                                <img src="{{$brand->photo_sm}}">
+                                <img src="{{$brand->photo_sm}}" alt="Foto marca {{$brand->brand_name}}" >
                             </div>
                         </div>
                     </a>
@@ -37,6 +39,8 @@
             <a class="open-all" href="{{route('all.car')}}">Ver todas las marcas</a>
         </section>
     @endif
+{{--        FIN SECCION MARCAS       --}}
+{{--        PRINCIPIO SECCION MODELOS DE COCHES       --}}
     @if($models->count() > 0)
         <section>
             <h2>Modelos mas populares</h2>
@@ -46,7 +50,7 @@
                         <div class="card-25">
                             <h3>{{$model->model}}</h3>
                             <div class="image-box-sm">
-                                <img src="{{$model->photo_sm}}">
+                                <img src="{{$model->photo_sm}}" alt="Foto modelo de coche {{$model->model_name}}">
                             </div>
                         </div>
                     </a>
@@ -55,4 +59,5 @@
             <a class="open-all" href="{{route('all.car')}}">Ver todas los modelos</a>
         </section>
     @endif
+{{--        FIN SECCION MODELOS DE COCHES       --}}
 @endsection
